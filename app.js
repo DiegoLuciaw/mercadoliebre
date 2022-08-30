b57c92b9f6ffc3e3eb40dd942d05c6cbed6a87ce
DiegoLuciaw <luciawdiego@gmail.com>
2022-08-30 15:49:31 +0200

    primer commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+const publicPath = path.join(__dirname, './public');
+app.use(express.static(publicPath));
+
+app.listen(process.env.PORT || 3000, function () {
+    console.log("Servidor corriendo en puerto 3000");
+  });
+
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/home.html'));
+})
+
